c358531ad4300a5933f7cb064f1555192dbf357c
IlikeChooros <pawel.smolnicki@gmail.com>
2023-03-11 19:34:10 +0100

    Working AI

--- src/Tester.py
+++ src/Tester.py
@@ -1,7 +1,22 @@
-import random
+import random, os
 
 from data_converter import DataConverter
 from NeuralNetwork import NeuralNetwork
+
+
+def progress_bar(idx, total):
+    lenght = 20
+
+    done = round((idx+1)/total*lenght)
+
+    togo = lenght - done
+
+    done_str = '█'*int(done)
+    togo_str = '░'*int(togo)
+
+    print(f'Progress: {done_str}{togo_str} {round((idx+1)/total*100)}% ', end='\r')
+
+
 
 
 
@@ -11,24 +26,32 @@ class Tester:
         self.learn_batch = []
     
     def teach(self):
-        
+        global progress_bar
+        repeat = 50
+        max_batch = len(self.learn_batch)*repeat
         itr = 0
-        for data in self.learn_batch:
-            itr += 1
-            if itr == 50:
-                self.__network.apply(1.3, itr)
-                itr = 1
-            self.__network.learn(data)
+
+        for i in range(repeat):
+            for data in self.learn_batch:
+                itr += 1
+                if itr % repeat == 0:
+                    progress_bar(itr, max_batch)
+                    self.__network.apply(1.3, repeat)
+                self.__network.learn(data)
             
 
     
     def check(self):
 
+        os.system('cls')
+
         x = float(input("Set x: "))
         y = float(input("Set y: "))
-        self.__network.network_output()
 
         self.__network.inputs = [x, y]
+
+        self.__network.network_output()
+        
         print("Network: "+str(self.__network.output[:]) + " X < Y  (1 0)")
 
         match self.__network.classify():
@@ -42,9 +65,11 @@ class Tester:
         while True:
             x = float(input("Set x: "))
             y = float(input("Set y: "))
-            self.__network.network_output()
 
             self.__network.inputs = [x, y]
+
+            self.__network.network_output()
+
             print("Network: "+str(self.__network.output[:]))
 
             match self.__network.classify():
@@ -65,8 +90,8 @@ class Tester:
 
             for i in range(1000):
                 
-                x = random.random()*5
-                y = random.random()*5
+                x = random.randint(0, 10)*0.5
+                y = random.randint(0, 10)*0.5
 
 
                 string = str(x) + " " + str(y)
@@ -75,7 +100,6 @@ class Tester:
 
                 else:
                     string += " 0 1\n"
-
 
                 file.write(string)
 
@@ -86,7 +110,7 @@ if __name__ == "__main__":
 
     data = DataConverter()
     
-    tester = Tester(NeuralNetwork([2,5,5,2]))
+    tester = Tester(NeuralNetwork([2,50,5,2]))
     tester.create_point_test("src/tests/triangle.txt", lambda x,y: x < y)
     tester.learn_batch = data.list_to_Data(data.prepare_data_txt("src/tests/triangle.txt"), 2, 2)
     tester.teach()
